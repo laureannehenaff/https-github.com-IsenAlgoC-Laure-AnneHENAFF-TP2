3e4aa7f71e9a2e698e72035fd557daa889b9390c
laureannehenaff <71849436+laureannehenaff@users.noreply.github.com>
2020-10-08 18:01:25 +0200

    Add files via upload

--- boucle.c
+++ boucle.c
@@ -9,27 +9,28 @@ int somme3 = 0 ;
 
 int main() {
     printf("la valeur de n est");
-    scanf_s("%hu",&n);
+    scanf_s("%hu",&n);                                  //On demande a l'utilisateur de rentrer n
     for ( int i=1 ; i<= n ; i++) { somme1 = somme1 + i; };
     printf("la somme des %d premiers entiers vaut %d",n,somme1);
 
-    while (a<=100) {somme2 = somme2 + a; a = a+1 ; 
+    while (a<=n) {somme2 = somme2 + a; a = a+1 ; 
         if ((somme2-565535) >= a) {
-            break; 
+            break;                                  //On sort de la boucle while car il y a un dépassement de capacité.
             printf("Dépassement de capacité");
         } 
     } ;
-    printf("\nla somme des 100 premiers entiers vaut %d",somme2);
+    printf("\nla somme des %d premiers entiers vaut %d",n,somme2);
 
 
     do {somme3=somme3 + b; b=b+1; 
         if ((somme2-565535) >= a) {
             printf("\nIl y a dépassement de capacité, il faut changer n"); 
-            scanf_s("%hu",&n);
+            printf("\nEntrez un nouveau n \n");     //on saisit une nouvelle valeur pour n tant qu'il n'y aura pas de n correct 
+            scanf_s("%hu",&n);                      //permettant de réaliser la somme des npremiers entiers.
         }
     } 
-    while (b<=100 );
-    printf("\nla somme des 100 premiers entiers vaut %d",somme3);
+    while (b<=n );
+    printf("\nla somme des %d premiers entiers vaut %d",n,somme3);
 }
 
 
